0432b8486856c6155cd73c268df30d46fdbec1ed
jieniyu <jieni.yu42@gmail.com>
2023-12-29 16:40:12 +0100

    initial commit

---
+++ src/main/java/com/ra/onlinejudge/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.ra.onlinejudge.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ra.onlinejudge.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
