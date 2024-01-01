c4230593511f3b54d96b9915b307d227ddb08e1e
jieniyu <jieni.yu42@gmail.com>
2024-01-01 23:57:43 +0100

    generate with myBatis

---
+++ src/main/java/com/ra/onlinejudge/mapper/QuestionMapper.java
@@ -0,0 +1,18 @@
+package com.ra.onlinejudge.mapper;
+
+import com.ra.onlinejudge.model.entity.Question;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author jieniyu
+* @description 针对表【question(题目)】的数据库操作Mapper
+* @createDate 2024-01-01 23:53:40
+* @Entity com.ra.onlinejudge.model.entity.Question
+*/
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+
---
+++ src/main/java/com/ra/onlinejudge/mapper/QuestionSubmitMapper.java
@@ -0,0 +1,18 @@
+package com.ra.onlinejudge.mapper;
+
+import com.ra.onlinejudge.model.entity.QuestionSubmit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author jieniyu
+* @description 针对表【question_submit(题目提交)】的数据库操作Mapper
+* @createDate 2024-01-01 23:54:29
+* @Entity com.ra.onlinejudge.model.entity.QuestionSubmit
+*/
+public interface QuestionSubmitMapper extends BaseMapper<QuestionSubmit> {
+
+}
+
+
+
+
